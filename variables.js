9a34a91532cfbfc7d1de86f6e041765520f165e1
entorres1 <entorres@albany.edu>
2025-03-24 20:05:05 -0400

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Using let
+
+let name = "Crystal";
+let age = "unknown";
+let isStudent = true;
+
+
+document.getElementById("demo1").innerHTML =
+"Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+//var city = "Albany";	// Function-scoped
+//let state = "NY";	//Block-scoped
+//const country = "USA";	// Cannot be reassigned
+
+//document.getElementById("demo2").innerHTML =
+//"Location: " + city + ", " + state +, " + country;
+
